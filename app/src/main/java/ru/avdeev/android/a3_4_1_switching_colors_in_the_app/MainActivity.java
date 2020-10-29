4054dc0a5beda67bc01aaa2702c11dfe6d2cf85c
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-10-29 20:32:59 +0300

    Initial commit

---
+++ app/src/main/java/ru/avdeev/android/a3_4_1_switching_colors_in_the_app/MainActivity.java
@@ -0,0 +1,65 @@
+package ru.avdeev.android.a3_4_1_switching_colors_in_the_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner languageSpinner;
+    String choiceLanguage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        languageSpinner=findViewById(R.id.languageSpinner);
+        givLanguage();
+    }
+
+    public void onClick(View view) {
+        if (choiceLanguage.equals("Russian")) {
+            Locale locale = new Locale("ru");
+            Configuration config = new Configuration();
+            config.setLocale(locale);
+            getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+            recreate();
+        } else {
+            if (choiceLanguage.equals("Английский")) {
+                Locale locale = new Locale("en");
+                Configuration config = new Configuration();
+                config.setLocale(locale);
+                getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                recreate();
+            }
+        }
+    }
+    private void givLanguage () {
+        ArrayAdapter<CharSequence> adapterLanguages = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
+        adapterLanguages.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapterLanguages);
+
+        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String[] choiceLanguages = getResources().getStringArray(R.array.languages);
+                choiceLanguage = choiceLanguages[i];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //showMyMessage("Ничего не выбрано", MainActivity.this);
+            }
+        });
+    }
+
+    private void givColor() {
+
+    }
+}
