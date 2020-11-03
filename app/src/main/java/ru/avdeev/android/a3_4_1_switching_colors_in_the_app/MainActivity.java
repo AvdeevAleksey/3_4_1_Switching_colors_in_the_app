0f2437590110817886844d66bc7e48d4fb46fca0
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-11-03 18:52:35 +0300

    The color change works. The black background doesn't work.

--- app/src/main/java/ru/avdeev/android/a3_4_1_switching_colors_in_the_app/MainActivity.java
+++ app/src/main/java/ru/avdeev/android/a3_4_1_switching_colors_in_the_app/MainActivity.java
@@ -1,7 +1,9 @@
 package ru.avdeev.android.a3_4_1_switching_colors_in_the_app;
 
+import androidx.annotation.StyleRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
@@ -15,15 +17,13 @@ public class MainActivity extends AppCompatActivity {
     Spinner languageSpinner;
     Spinner colorSpinner;
     String choiceLanguage;
-    String choiceColor;
-    private static int sTheme;
-    public final static int THEME_GREEN = 0;
-    public final static int THEME_BLUE = 1;
-    public final static int THEME_BACK = 2;
+    int choiceColor;
+    private static @StyleRes int sTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(sTheme);
         setContentView(R.layout.activity_main);
         languageSpinner=findViewById(R.id.languageSpinner);
         colorSpinner=findViewById(R.id.colorSpinner);
@@ -47,6 +47,22 @@ public class MainActivity extends AppCompatActivity {
                 recreate();
             }
         }
+        switch (choiceColor) {
+            case 0:
+                sTheme = R.style.ThemeGreen;
+                recreate();
+                break;
+            case 1:
+                sTheme = R.style.ThemeBlue;
+                recreate();
+                break;
+            case 2:
+                sTheme = R.style.ThemeBlack;
+                recreate();
+                break;
+            default:
+                break;
+        }
     }
     private void givLanguage () {
         ArrayAdapter<CharSequence> adapterLanguages = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
@@ -62,7 +78,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-                //showMyMessage("Ничего не выбрано", MainActivity.this);
             }
         });
     }
@@ -76,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 String[] choiceColors = getResources().getStringArray(R.array.colors);
-                choiceColor = choiceColors[i];
+                choiceColor = i;
             }
 
             @Override
@@ -84,8 +99,5 @@ public class MainActivity extends AppCompatActivity {
                 //showMyMessage("Ничего не выбрано", MainActivity.this);
             }
         });
-    }
-    private void setColor() {
-
     }
 }
